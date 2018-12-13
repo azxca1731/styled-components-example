804c258a11d9c2402a134e533613cfc5cb15d1ba
azxca1731 <azxca1731@gmail.com>
2018-12-13 22:32:14 +0900

    make styled button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import styled from "styled-components";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hello
+        <Button>Hello</Button>
+        <Button>Styled Component</Button>
       </div>
     );
   }
 }
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  -webkkit-appearance: none;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+`;
 
 export default App;
