a88ceaed27a62c7e2f05ffe2dca2ed0e4ba074bb
azxca1731 <azxca1731@gmail.com>
2018-12-13 22:39:41 +0900

    make container

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import styled from "styled-components";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Container>
         <Button>Hello</Button>
-        <Button>Styled Component</Button>
-      </div>
+        <Button danger>Styled Component</Button>
+      </Container>
     );
   }
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: pink;
+`;
 
 const Button = styled.button`
   border-radius: 50px;
@@ -23,6 +29,7 @@ const Button = styled.button`
   &:focus {
     outline: none;
   }
+  background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
 `;
 
 export default App;
